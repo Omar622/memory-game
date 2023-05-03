5817afac5e73f52c5e45c70ecca1f4ef92ee8c89
Omar622 <omaramin622@gmail.com>
2023-05-03 16:45:59 +0300

    Set content width

--- src/pages/View.js
+++ src/pages/View.js
@@ -6,14 +6,14 @@ function View(props) {
 
   return (
     <div className={style.view}>
-      <div>
-      <h1>Memory Game</h1>
-      <h3>Best: {bestScore}</h3>
-      <h3>Score: {currentScore}</h3>
-      <GameBoard
-        chosenImages={chosenImages}
-        handleChooseImage={handleChooseImage}
-      ></GameBoard>
+      <div className={style.viewChild}>
+        <h1>Memory Game</h1>
+        <h3>Best: {bestScore}</h3>
+        <h3>Score: {currentScore}</h3>
+        <GameBoard
+          chosenImages={chosenImages}
+          handleChooseImage={handleChooseImage}
+        ></GameBoard>
       </div>
     </div>
 
